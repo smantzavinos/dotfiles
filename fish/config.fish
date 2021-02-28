6f4c580c168e421c7d2f9d2f68f7feed08c2c67e
Spiros Mantzavinos <smantzavinos@gmail.com>
2021-02-28 12:09:13 -0800

    Added config.fish and fish_plugins

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+
+# Adapted from https://github.com/fish-shell/fish-shell/issues/4434#issuecomment-332626369 
+# only run in interactive (not automated SSH for example) 
+if status is-interactive 
+# don't nest inside another tmux 
+and not set -q TMUX 
+  # Adapted from https://unix.stackexchange.com/a/176885/347104 
+  # Create session 'main' or attach to 'main' if already exists. 
+  tmux new-session -A -s main 
+end
+
+
