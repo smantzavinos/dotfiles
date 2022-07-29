29d4e593357df3672b4b0b08cfb1aa5d9199c0b5
smantzavinos <smantzavinos@globusmedical.com>
2022-07-29 10:56:13 -0400

    Add emacs local init file so symlinks are not needed.
    README updates still needed.

---
+++ .emacs.d/init_local.el
@@ -0,0 +1,12 @@
+;; Following code taken and modified from:
+;; https://emacs.stackexchange.com/questions/12881/how-do-i-set-a-different-location-for-the-dot-emacs-emacs-file-on-windows-7
+
+;; Place this file in C:\Users\Username\AppData\Roaming and point to the appropriate files
+(setenv "HOME" "C:/path/to/home/dir")
+(setenv "ORG_ROOT" "C:/path/to/notes/org")
+
+(setq user-emacs-directory (concat (getenv "HOME") "/dotfiles/.emacs.d"))
+(setq user-init-file (concat (getenv "HOME") "/dotfiles/.emacs.d/init.el"))
+(setq default-directory (getenv "HOME"))
+
+(load user-init-file)
