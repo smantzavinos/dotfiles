7ce7c41c9a2a573b9f800f28eb5d6e71839b2314
smantzavinos <smantzavinos@globusmedical.com>
2022-08-01 11:12:51 -0400

    Fix config warnings. Config ivy-prescient mode.
    Also treat themes as safe.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -41,6 +41,9 @@
 
 (set-face-attribute `default nil :font "Consolas NF")
 
+;; Treat all themes as safe
+(setq custom-safe-themes t)
+
 (load-theme 'tango-dark)
 
 ;; Set up modus theme
@@ -90,6 +93,51 @@
 
 ;; Many settins from:
 ;; https://github.com/daviwil/dotfiles/blob/9776d65c4486f2fa08ec60a06e86ecb6d2c40085/Emacs.org#git
+
+;; evil mode
+(defun sm/evil-hook ()
+  (dolist (mode '(custom-mode
+                  eshell-mode
+                  git-rebase-mode
+                  erc-mode
+                  circe-server-mode
+                  circe-chat-mode
+                  circe-query-mode
+                  sauron-mode
+                  term-mode))
+  (add-to-list 'evil-emacs-state-modes mode)))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  (setq evil-respect-visual-line-mode t)
+  (setq evil-undo-system 'undo-tree)
+  :config
+  (add-hook 'evil-mode-hook 'sm/evil-hook)
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :init
+  (setq evil-collection-company-use-tng nil)  ;; Is this a bug in evil-collection?
+  :custom
+  (evil-collection-outline-bind-tab-p nil)
+  :config
+  (delete 'lispy evil-collection-mode-list)
+  (delete 'org-present evil-collection-mode-list)
+  (evil-collection-init))
 
 ;; General
 (use-package general
@@ -214,66 +262,23 @@
   "pc"  'projectile-compile-project
   "pd"  'projectile-dired)
 
-;; evil mode
-(defun sm/evil-hook ()
-  (dolist (mode '(custom-mode
-                  eshell-mode
-                  git-rebase-mode
-                  erc-mode
-                  circe-server-mode
-                  circe-chat-mode
-                  circe-query-mode
-                  sauron-mode
-                  term-mode))
-  (add-to-list 'evil-emacs-state-modes mode)))
-
 (use-package undo-tree
   :init
   (global-undo-tree-mode 1))
-
-(use-package evil
-  :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump nil)
-  (setq evil-respect-visual-line-mode t)
-  (setq evil-undo-system 'undo-tree)
-  :config
-  (add-hook 'evil-mode-hook 'sm/evil-hook)
-  (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-
-  ;; Use visual line motions even outside of visual-line-mode buffers
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-
-  (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal))
-
-(use-package evil-collection
-  :after evil
-  :init
-  (setq evil-collection-company-use-tng nil)  ;; Is this a bug in evil-collection?
-  :custom
-  (evil-collection-outline-bind-tab-p nil)
-  :config
-  (delete 'lispy evil-collection-mode-list)
-  (delete 'org-present evil-collection-mode-list)
-  (evil-collection-init))
 
 ;; ivy
 (use-package counsel)
 
 
-(straight-use-package 'prescient)
-(straight-use-package 'ivy-prescient
-		      :config
-		      (ivy-prescient-mode 1))
-(straight-use-package 'company-prescient
-		      :config
-		      (company-prescient-mode 1))
+(use-package prescient)
+(use-package ivy-prescient
+    :straight t
+    :config
+    (ivy-prescient-mode 1))
+
+;; (straight-use-package 'company-prescient)
+    ;; :config
+    ;; (company-prescient-mode 1))
 ;; (straight-use-package 'selectrum-prescient
 ;; 		      :config
 ;; 		      (selectrum-prescient-mode 1))
@@ -375,24 +380,24 @@
 
 ;; C++ dev. lsp.
 ;; from: https://lupan.pl/dotemacs/
-(use-package cc-mode
-  :bind (:map c-mode-map
-         ("C-i" . company-indent-or-complete-common)
-         :map c++-mode-map
-         ("C-i" . company-indent-or-complete-common))
-  :init
-  (setq-default c-basic-offset 8))
+;; (use-package cc-mode
+;;   :bind (:map c-mode-map
+;;          ("C-i" . company-indent-or-complete-common)
+;;          :map c++-mode-map
+;;          ("C-i" . company-indent-or-complete-common))
+;;   :init
+;;   (setq-default c-basic-offset 8))
 
 ;; web mode
 ;; from: https://lupan.pl/dotemacs/
 (use-package web-mode
   :mode "\\.\\([jt]sx\\)\\'")
 
-(use-package js
-  :bind (:map js-mode-map
-         ([remap js-find-symbol] . xref-find-definitions))
-  :init
-  (setq js-indent-level 4))
+;; (use-package js
+;;   :bind (:map js-mode-map
+;;          ([remap js-find-symbol] . xref-find-definitions))
+;;   :init
+;;   (setq js-indent-level 4))
 
 (use-package typescript-mode
   :defer)
