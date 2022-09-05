1b23371484d603653764a955d4f8bcba4f470c2e
Spiros Mantzavinos <smantzavinos@gmail.com>
2022-09-05 14:22:26 -0400

    Improve find file shortcuts.
    Remove need for emacs 28 by replacing
    file-name-concat with f-join.
    
    Replaces find-file with counsel-find-file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -52,6 +52,9 @@
 
 (set-face-attribute `default nil :font "Consolas NF")
 
+;; File path utility library
+(use-package f)
+
 ;; Treat all themes as safe
 (setq custom-safe-themes t)
 
@@ -237,18 +240,19 @@
   "bd"  'kill-buffer)
 
 (defun sm/find-file-in-directory (dir)
-  "Interactively launch find-file in the given directory."
+  "Interactively launch counsel-find-file in the given directory."
   (interactive)
-  (find-file dir))
+  (cd dir)
+  (counsel-find-file))
 
 (sm/leader-key-def
   "f"   '(:ignore t :which-key "files")
   "fd"  (lambda () (interactive) (find-file user-init-file))
   "fn"  (lambda () (interactive) (sm/find-file-in-directory sm/notes-directory))
-  "fp"  (lambda () (interactive) (sm/find-file-in-directory (file-name-concat sm/notes-directory "1_projects")))
-  "fa"  (lambda () (interactive) (sm/find-file-in-directory (file-name-concat sm/notes-directory "2_areas")))
-  "fr"  (lambda () (interactive) (sm/find-file-in-directory (file-name-concat sm/notes-directory "3_resources")))
-  "ft"  (lambda () (interactive) (sm/find-file-in-directory (file-name-concat sm/notes-directory "4_archive")))
+  "fp"  (lambda () (interactive) (sm/find-file-in-directory (f-join sm/notes-directory "1_projects")))
+  "fa"  (lambda () (interactive) (sm/find-file-in-directory (f-join sm/notes-directory "2_areas")))
+  "fr"  (lambda () (interactive) (sm/find-file-in-directory (f-join sm/notes-directory "3_resources")))
+  "ft"  (lambda () (interactive) (sm/find-file-in-directory (f-join sm/notes-directory "4_archive")))
   "ff"  'counsel-find-file)
 
 (sm/leader-key-def
