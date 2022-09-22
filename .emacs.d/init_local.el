c43cfc5c20efcdf2ade4d8ffab53ca8538bbf1b1
Spiros Mantzavinos <smantzavinos@globusmedical.com>
2022-09-22 08:44:01 -0400

    Remove unused code from init_local.el

--- .emacs.d/init_local.el
+++ .emacs.d/init_local.el
@@ -3,7 +3,6 @@
 
 ;; Place this file in C:\Users\Username\AppData\Roaming and point to the appropriate files
 (setenv "HOME" "C:/path/to/home/dir")
-(setenv "ORG_ROOT" "C:/path/to/notes/org")
 
 (defvar sm/notes-directory "c:/path/to/notes"
   "Root directory of PARA style notes.")
@@ -11,8 +10,5 @@
 (setq user-emacs-directory (concat (getenv "HOME") "/dotfiles/.emacs.d"))
 (setq user-init-file (concat (getenv "HOME") "/dotfiles/.emacs.d/init.el"))
 (setq default-directory (getenv "HOME"))
-
-;; Directory to look for org files in to add to agenda
-(setq org-agenda-files (directory-files-recursively (concat (getenv "ORG_ROOT") "/tasks") "\\.org$"))
 
 (load user-init-file)
