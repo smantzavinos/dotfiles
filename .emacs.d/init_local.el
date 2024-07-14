02fe1ec3a0005a421fecc314c2fc35ee60f83945
Spiros Mantzavinos <smantzavinos@gmail.com>
2024-07-13 21:47:36 -0400

    Set font size from init_local.el variable.

--- .emacs.d/init_local.el
+++ .emacs.d/init_local.el
@@ -14,6 +14,11 @@
 (setq user-config-file (concat (getenv "HOME") "/dotfiles/.emacs.d/config.org"))
 (setq default-directory (getenv "HOME"))
 
+;; Set the default font size.
+;; Units are 1/10th of a point.
+;; Set to 120 for font size of 12.
+(setq my-default-font-size 120)
+
 ;; Set path to plantuml.jar file to render plantuml source blocks.
 ;; If installed with chocolatey on Windows, the following line is likely correct
 ;; (setq org-plantuml-jar-path "C:/ProgramData/chocolatey/lib/plantuml/tools/plantuml.jar")
