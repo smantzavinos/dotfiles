f093606a361395f2476f9efd5dfb36a743aaf3b3
Spiros Mantzavinos <smantzavinos@gmail.com>
2024-08-19 18:05:21 +0000

    chore: update nixpkgs to use nixos-unstable branch

--- nix/home/flake.nix
+++ nix/home/flake.nix
@@ -2,7 +2,7 @@
     description = "My Home Manager Flake";
 
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
         home-manager = {
             url = "github:nix-community/home-manager/release-23.11";
             inputs.nixpkgs.follows = "nixpkgs";
