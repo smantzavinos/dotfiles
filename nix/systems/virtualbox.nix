8e25c562a53bf80c566cf55c2f5b81edb4cf3e27
Spiros Mantzavinos <smantzavinos@gmail.com>
2024-11-18 15:11:59 -0500

    Install nodejs in virtualbox

--- nix/systems/virtualbox.nix
+++ nix/systems/virtualbox.nix
@@ -19,7 +19,7 @@
   # List packages installed in system profile. To search, run:
   # \$ nix search wget
   environment.systemPackages = with pkgs; [
-    wget vim git
+    wget vim git nodejs_22
   ];
 
 
