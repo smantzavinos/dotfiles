5fcc637f5c0605fbbbd5320766a80ad3736faa3c
Spiros Mantzavinos <smantzavinos@gmail.com>
2024-11-20 17:21:20 -0500

    install vscode

--- nix/home/home.nix
+++ nix/home/home.nix
@@ -150,6 +150,16 @@
       userEmail = "smantzavinos@gmail.com";
     };
 
+    programs.vscode = {
+      enable = true;
+      extensions = with pkgs.vscode-extensions; [
+        dracula-theme.theme-dracula
+        vscodevim.vim
+        yzhang.markdown-all-in-one
+        svelte.svelte-vscode
+      ];
+    };
+
     programs.neovim = {
       enable = true;
       vimAlias = true;
