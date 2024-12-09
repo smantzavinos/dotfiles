13144ee0a31be1c39ca004b52f672a967067091d
Spiros Mantzavinos <smantzavinos@gmail.com>
2024-12-09 06:39:46 -0500

    Finish nvim-web-devicons config move

--- nix/home/nvim/lua/nvim-web-devicons.lua
+++ nix/home/nvim/lua/nvim-web-devicons.lua
@@ -3,7 +3,13 @@ return {
     "nvim-tree/nvim-web-devicons",
     config = function()
       require('nvim-web-devicons').setup({
+        -- globally enable "strict" selection of icons - icon will be looked up in
+        -- different tables, first by filename, and if not found by extension; this
+        -- prevents cases when file doesn't have any extension but still gets some icon
+        -- because its name happened to match some extension (default to false)
         strict = true,
+        -- same as `override` but specifically for overrides by extension
+        -- takes effect when `strict` is true
         override_by_filename = {
          [".gitignore"] = {
            icon = "",
