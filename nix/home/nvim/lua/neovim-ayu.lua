26a9c56ae683134c97209108aaef01a6aeaf92fb
Spiros Mantzavinos <smantzavinos@gmail.com>
2025-03-25 06:53:18 -0400

    Update neovim-ayu to use catppuccin theme.

--- nix/home/nvim/lua/neovim-ayu.lua
+++ nix/home/nvim/lua/neovim-ayu.lua
@@ -1,12 +1,33 @@
 return {
   {
-    "Shatur/neovim-ayu",
+    "catppuccin/nvim",
+    name = "catppuccin",
     config = function()
       vim.opt.termguicolors = true
-      require('ayu').setup({
-          mirage = true,
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        background = {
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = false,
+        term_colors = true,
+        integrations = {
+          cmp = true,
+          nvimtree = true,
+          treesitter = true,
+          dap = {
+            enabled = true,
+            enable_ui = true,
+          },
+          native_lsp = {
+            enabled = true,
+          },
+          neogit = true,
+          which_key = true,
+        },
       })
-      require('ayu').colorscheme()
+      vim.cmd.colorscheme "catppuccin"
     end
   }
 }
