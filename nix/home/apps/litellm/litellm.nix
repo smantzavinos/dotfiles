65714f9aac327530f79ff99227dd72d5a8218008
Spiros Mantzavinos <smantzavinos@gmail.com>
2025-04-18 23:25:09 -0400

    fix: Update OPENAI_API_KEY secret reference in LiteLLM configuration

--- nix/home/apps/litellm/litellm.nix
+++ nix/home/apps/litellm/litellm.nix
@@ -9,7 +9,7 @@
       ExecStart = ''
         ${pkgs.docker}/bin/docker run --rm \
           -v /nix/home/apps/litellm/config.yaml:/app/config.yaml \
-          -e OPENAI_API_KEY=$(sops -d ${config.sops.secrets.aiApiKeys.sopsFile} | yq eval '.OPENAI_API_KEY' -) \
+          -e OPENAI_API_KEY=$(sops -d ${config.sops.secrets.OPENAI_API_KEY.sopsFile} | yq eval '.OPENAI_API_KEY' -) \
           -e MODEL='gpt-4o' \
           -p 4000:4000 \
           ghcr.io/berriai/litellm:main-latest --config /app/config.yaml --detailed_debug
