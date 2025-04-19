0ae5303e027c49ae7d76626557fc49759e85f172
Spiros Mantzavinos <smantzavinos@gmail.com>
2025-04-18 23:00:17 -0400

    fix: Update LiteLLM Docker config to use OpenAI API key instead of Azure

--- nix/home/apps/litellm/litellm.nix
+++ nix/home/apps/litellm/litellm.nix
@@ -9,7 +9,7 @@
       ExecStart = ''
         ${pkgs.docker}/bin/docker run --rm \
           -v /nix/home/apps/litellm/config.yaml:/app/config.yaml \
-          -e AZURE_API_KEY_EU=$(cat ${config.sops.secrets.aiApiKeys.path} | yq eval '.AZURE_API_KEY_EU' -) \
+          -e OPENAI_API_KEY=$(cat ${config.sops.secrets.aiApiKeys.path} | yq eval '.OPENAI_API_KEY' -) \
           -e MODEL='gpt-4o' \
           -p 4000:4000 \
           ghcr.io/berriai/litellm:main-latest --config /app/config.yaml --detailed_debug
