098715fc7a09ef70a6635644fc037f57e0cd0dc5
Spiros Mantzavinos <smantzavinos@gmail.com>
2025-04-21 06:56:47 -0400

    Fix openai api calls

--- nix/home/nvim/lua/avante.lua
+++ nix/home/nvim/lua/avante.lua
@@ -12,7 +12,7 @@ return {
             model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
             timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
             temperature = 0,
-            max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+            -- max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
             --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
           },
         })
