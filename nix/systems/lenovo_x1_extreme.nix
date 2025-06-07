4a6f4839ddea6cdf914a1e07a0374a5302da9303
Spiros Mantzavinos <smantzavinos@gmail.com>
2025-06-07 11:43:16 -0400

    fix: update hostname to "x1" in Lenovo X1 Extreme configuration

--- nix/systems/lenovo_x1_extreme.nix
+++ nix/systems/lenovo_x1_extreme.nix
@@ -91,7 +91,7 @@
   #   "/crypto_keyfile.bin" = null;
   # };
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "x1"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
