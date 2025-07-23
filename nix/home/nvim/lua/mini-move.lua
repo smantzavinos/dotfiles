08ce3424c38e1c5d8943a052243d474b69b8c35e
Spiros Mantzavinos <smantzavinos@gmail.com>
2025-07-23 07:03:23 -0400

    Remove erroneous config for mini-move

--- nix/home/nvim/lua/mini-move.lua
+++ nix/home/nvim/lua/mini-move.lua
@@ -4,9 +4,6 @@ return {
     version = false,
     config = function()
       require("mini.move").setup()
-      local map = vim.keymap.set
-      map({ "n", "v" }, "<M-j>", "<Plug>(MiniMoveLineDown)")
-      map({ "n", "v" }, "<M-k>", "<Plug>(MiniMoveLineUp)")
     end,
   },
 }
