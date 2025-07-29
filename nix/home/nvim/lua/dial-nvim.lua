eca7e299a16357f147d6e2d965947a172482744a
Spiros Mantzavinos <smantzavinos@gmail.com>
2025-07-29 07:12:08 -0400

    Set date increment/decrement keybindings

--- nix/home/nvim/lua/dial-nvim.lua
+++ nix/home/nvim/lua/dial-nvim.lua
@@ -13,10 +13,10 @@ return {
       })
       local map = vim.keymap.set
       local dm = require("dial.map")
-      map("n", "<C-.>", dm.inc_normal(), { noremap = true })
-      map("n", "<C-,>", dm.dec_normal(), { noremap = true })
-      map("v", "<C-.>", dm.inc_visual(), { noremap = true })
-      map("v", "<C-,>", dm.dec_visual(), { noremap = true })
+      map("n", "g+", dm.inc_normal(), { noremap = true })
+      map("n", "g-", dm.dec_normal(), { noremap = true })
+      map("v", "g+", dm.inc_visual(), { noremap = true })
+      map("v", "g-", dm.dec_visual(), { noremap = true })
     end,
   },
 }
