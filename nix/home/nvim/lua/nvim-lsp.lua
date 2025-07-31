e6915450df15032eccc9e2de3a04946e1bde4128
Spiros Mantzavinos <smantzavinos@gmail.com>
2025-07-30 21:49:28 -0400

    lsp keybindings

--- nix/home/nvim/lua/nvim-lsp.lua
+++ nix/home/nvim/lua/nvim-lsp.lua
@@ -4,15 +4,40 @@ return {
         config = function()
             local lspconfig = require('lspconfig')
 
+            -- Common LSP keybindings function
+            local on_attach = function(client, bufnr)
+                local opts = { noremap = true, silent = true, buffer = bufnr }
+                
+                -- Navigation
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
+                
+                -- Documentation and help
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                
+                -- Code actions and refactoring
+                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+                vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+                vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format({ async = true }) end, opts)
+                
+                -- Diagnostics
+                vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+                vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+                vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+                vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+            end
+
             -- TypeScript/JavaScript LSP
-            lspconfig.ts_ls.setup{}
+            lspconfig.ts_ls.setup{
+                on_attach = on_attach,
+            }
 
             -- Svelte LSP
             lspconfig.svelte.setup {
-                on_attach = function(client, bufnr)
-                    -- Add your custom on_attach logic here, if needed
-                    -- For example, you can set keybindings for LSP features
-                end,
+                on_attach = on_attach,
                 flags = {
                     debounce_text_changes = 150,
                 }
@@ -21,6 +46,7 @@ return {
             -- Nix LSP
             lspconfig.nixd.setup({
                 cmd = { "nixd" },
+                on_attach = on_attach,
                 settings = {
                     nixd = {
                         nixpkgs = {
