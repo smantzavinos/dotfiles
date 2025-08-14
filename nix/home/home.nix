0f45bcd198180ee31e30bc81b5621d6adba9c88c
Spiros Mantzavinos <smantzavinos@gmail.com>
2025-08-13 21:41:34 -0400

    Starship working with catpuccin-powerline preset

--- nix/home/home.nix
+++ nix/home/home.nix
@@ -249,197 +249,10 @@
 
     programs.starship = {
       enable = true;
-      settings = {
-        "$schema" = "https://starship.rs/config-schema.json";
-        
-        format = "[](red)$os$username[](bg:peach fg:red)$directory[](bg:yellow fg:peach)$git_branch$git_status[](fg:yellow bg:green)$c$rust$golang$nodejs$php$java$kotlin$haskell$python[](fg:green bg:sapphire)$conda[](fg:sapphire bg:lavender)$time[ ](fg:lavender)$cmd_duration$line_break$character";
-        
-        palette = "catppuccin_macchiato";
-        
-        os = {
-          disabled = false;
-          style = "bg:red fg:crust";
-          symbols = {
-            Windows = "";
-            Ubuntu = "󰕈";
-            SUSE = "";
-            Raspbian = "󰐿";
-            Mint = "󰣭";
-            Macos = "󰀵";
-            Manjaro = "";
-            Linux = "󰌽";
-            Gentoo = "󰣨";
-            Fedora = "󰣛";
-            Alpine = "";
-            Amazon = "";
-            Android = "";
-            Arch = "󰣇";
-            Artix = "󰣇";
-            CentOS = "";
-            Debian = "󰣚";
-            Redhat = "󱄛";
-            RedHatEnterprise = "󱄛";
-          };
-        };
-        
-        username = {
-          show_always = true;
-          style_user = "bg:red fg:crust";
-          style_root = "bg:red fg:crust";
-          format = "[ $user]($style)";
-        };
-        
-        directory = {
-          style = "bg:peach fg:crust";
-          format = "[ $path ]($style)";
-          truncation_length = 3;
-          truncation_symbol = "…/";
-          substitutions = {
-            Documents = "󰈙 ";
-            Downloads = " ";
-            Music = "󰝚 ";
-            Pictures = " ";
-            Developer = "󰲋 ";
-          };
-        };
-        
-        git_branch = {
-          symbol = "";
-          style = "bg:yellow";
-          format = "[[ $symbol $branch ](fg:crust bg:yellow)]($style)";
-        };
-        
-        git_status = {
-          style = "bg:yellow";
-          format = "[[($all_status$ahead_behind )](fg:crust bg:yellow)]($style)";
-        };
-        
-        nodejs = {
-          symbol = "";
-          style = "bg:green";
-          format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
-        };
-        
-        c = {
-          symbol = " ";
-          style = "bg:green";
-          format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
-        };
-        
-        rust = {
-          symbol = "";
-          style = "bg:green";
-          format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
-        };
-        
-        golang = {
-          symbol = "";
-          style = "bg:green";
-          format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
-        };
-        
-        php = {
-          symbol = "";
-          style = "bg:green";
-          format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
-        };
-        
-        java = {
-          symbol = " ";
-          style = "bg:green";
-          format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
-        };
-        
-        kotlin = {
-          symbol = "";
-          style = "bg:green";
-          format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
-        };
-        
-        haskell = {
-          symbol = "";
-          style = "bg:green";
-          format = "[[ $symbol( $version) ](fg:crust bg:green)]($style)";
-        };
-        
-        python = {
-          symbol = "";
-          style = "bg:green";
-          format = "[[ $symbol( $version)(\($virtualenv\)) ](fg:crust bg:green)]($style)";
-        };
-        
-        docker_context = {
-          symbol = "";
-          style = "bg:sapphire";
-          format = "[[ $symbol( $context) ](fg:crust bg:sapphire)]($style)";
-        };
-        
-        conda = {
-          symbol = "  ";
-          style = "fg:crust bg:sapphire";
-          format = "[$symbol$environment ]($style)";
-          ignore_base = false;
-        };
-        
-        time = {
-          disabled = false;
-          time_format = "%R";
-          style = "bg:lavender";
-          format = "[[  $time ](fg:crust bg:lavender)]($style)";
-        };
-        
-        line_break = {
-          disabled = true;
-        };
-        
-        character = {
-          disabled = false;
-          success_symbol = "[❯](bold fg:green)";
-          error_symbol = "[❯](bold fg:red)";
-          vimcmd_symbol = "[❮](bold fg:green)";
-          vimcmd_replace_one_symbol = "[❮](bold fg:lavender)";
-          vimcmd_replace_symbol = "[❮](bold fg:lavender)";
-          vimcmd_visual_symbol = "[❮](bold fg:yellow)";
-        };
-        
-        cmd_duration = {
-          show_milliseconds = true;
-          format = " in $duration ";
-          style = "bg:lavender";
-          disabled = false;
-          show_notifications = true;
-          min_time_to_notify = 45000;
-        };
-        
-        palettes.catppuccin_macchiato = {
-          rosewater = "#f4dbd6";
-          flamingo = "#f0c6c6";
-          pink = "#f5bde6";
-          mauve = "#c6a0f6";
-          red = "#ed8796";
-          maroon = "#ee99a0";
-          peach = "#f5a97f";
-          yellow = "#eed49f";
-          green = "#a6da95";
-          teal = "#8bd5ca";
-          sky = "#91d7e3";
-          sapphire = "#7dc4e4";
-          blue = "#8aadf4";
-          lavender = "#b7bdf8";
-          text = "#cad3f5";
-          subtext1 = "#b8c0e0";
-          subtext0 = "#a5adcb";
-          overlay2 = "#939ab7";
-          overlay1 = "#8087a2";
-          overlay0 = "#6e738d";
-          surface2 = "#5b6078";
-          surface1 = "#494d64";
-          surface0 = "#363a4f";
-          base = "#24273a";
-          mantle = "#1e2030";
-          crust = "#181926";
-        };
-      };
+    };
+
+    home.file.".config/starship.toml" = {
+      text = builtins.readFile ./starship.toml;
     };
 
     programs.vscode = {
@@ -842,7 +655,10 @@
   programs.kitty = {
     enable = true;
     themeFile = "Dracula";
-    # font = "JetBrainsMono Nerd Font";
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 10;
+    };
   };
 
   programs.alacritty = {
