bcbd1c22425807f7147f11bcd976fdd790c63e88
Spiros Mantzavinos <smantzavinos@gmail.com>
2025-09-26 06:59:55 -0400

    Use telescope instead of fzf-lua for ctrl-p/g/b keys

--- nix/home/nvim/lua/fzf-lua.lua
+++ nix/home/nvim/lua/fzf-lua.lua
@@ -5,9 +5,6 @@ return {
     -- optional for icon support
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
-      { "<c-p>", function() require('fzf-lua').files() end, { noremap = true, silent = true } },
-      { "<c-b>", function() require('fzf-lua').buffers() end, { noremap = true, silent = true } },
-      { "<c-g>", function() require('fzf-lua').live_grep_native() end, { noremap = true, silent = true } },
     },
     config = function()
       -- calling `setup` is optional for customization
